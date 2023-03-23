fa52d5d83c78a913bb78e7819d38fa63997495ba
Amitadmane123 <78906842+Amitadmane123@users.noreply.github.com>
2023-03-23 15:16:48 +0530

    recursion code

--- recursion/String.cpp
+++ recursion/String.cpp
@@ -112,4 +112,22 @@ using namespace std;
 
 // } 
 // Subsequences of a string
- 
+void printSubsequences(string str,string output,int i){
+    //base case
+    if(i>=str.length()){
+        cout<<output<<endl;
+        return;
+    }
+    //exclude
+    printSubsequences(str, output,i+1);
+    //include
+    output.push_back(str[i]);
+    printSubsequences(str, output, i+1);
+}
+int main(){
+    string str="abc";
+    string output ="";
+    int i=0;
+    printSubsequences(str, output, i);
+    return 0;
+}
