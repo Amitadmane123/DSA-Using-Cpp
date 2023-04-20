333eb854925a69e4abd786011fd13cb3863ab69a
Amitadmane123 <78906842+Amitadmane123@users.noreply.github.com>
2023-04-20 09:28:05 +0530

    divide and conquer code

---
+++ recursion/Backtracking/N-Queens.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void solve(vector<vector<int>> board,int col ,int n){
+    
+}
+int main(){
+    int n=4;
+    vector<vector<int>> board(n, vector<int>(n,0))
+    int col =0;
+    solve(board, col, n);
+}
--- recursion/merge sort.cpp
+++ recursion/merge sort.cpp
@@ -76,67 +76,67 @@ using namespace std;
 //     return 0;
 // }
 //QuickSort
-int partition(int arr[],int s,int e) {
-    //step 1: choose pivot element
-    int pivotIndex = s;
-    int pivotElement = arr[s];
-    //step 2: find right position for pivot element and place it there
-    int count = 0;
-    for(int i=s+1;i<=e;i++){
-        if(arr[i]<=pivotElement){
-            count++;
-        }
-    }
-    //jab main loop se bahar hua , toh mere paas pivot ki right position ka index ready h
-    int rightIndex = s +count;
-    swap(arr[pivotIndex], arr[rightIndex]);
-    pivotIndex = rightIndex;
+// int partition(int arr[],int s,int e) {
+//     //step 1: choose pivot element
+//     int pivotIndex = s;
+//     int pivotElement = arr[s];
+//     //step 2: find right position for pivot element and place it there
+//     int count = 0;
+//     for(int i=s+1;i<=e;i++){
+//         if(arr[i]<=pivotElement){
+//             count++;
+//         }
+//     }
+//     //jab main loop se bahar hua , toh mere paas pivot ki right position ka index ready h
+//     int rightIndex = s +count;
+//     swap(arr[pivotIndex], arr[rightIndex]);
+//     pivotIndex = rightIndex;
 
-    //step 3: left me chote and right me bade
-    int i=s;
-    int j=e;
-    while(i<pivotIndex && j> pivotElement){
-        while(arr[i]<pivotElement) {
-            i++;
-        }
-        while(arr[i]>pivotElement){
-            j--;
-        }
-        //2 case ho sakte h->
-        //a->you found the element to swap
-        //b->no need to swap
-        if(i<pivotIndex && j>pivotIndex)
-          swap(arr[i],arr[j]);
-    }
-    return pivotIndex;
+//     //step 3: left me chote and right me bade
+//     int i=s;
+//     int j=e;
+//     while(i<pivotIndex && j> pivotElement){
+//         while(arr[i]<pivotElement) {
+//             i++;
+//         }
+//         while(arr[i]>pivotElement){
+//             j--;
+//         }
+//         //2 case ho sakte h->
+//         //a->you found the element to swap
+//         //b->no need to swap
+//         if(i<pivotIndex && j>pivotIndex)
+//           swap(arr[i],arr[j]);
+//     }
+//     return pivotIndex;
 
 
-}
-void quickSort(int arr[],int s, int e){
-    //base case
-    if(s >= e)
-    return;
+// }
+// void quickSort(int arr[],int s, int e){
+//     //base case
+//     if(s >= e)
+//     return;
 
-    //partition logic
-    int p = partition(arr,s,e);
-    //recursion logic
-    //left
-    quickSort(arr,s,p-1);
-    //right
-    quickSort(arr,p+1,e);
+//     //partition logic
+//     int p = partition(arr,s,e);
+//     //recursion logic
+//     //left
+//     quickSort(arr,s,p-1);
+//     //right
+//     quickSort(arr,p+1,e);
 
-}
-int main(){
-    int arr[] = {8,1,3,4,20,50,30};
-    int n=7;
+// }
+// int main(){
+//     int arr[] = {8,1,3,4,20,50,30};
+//     int n=7;
 
-    int s = 0;
-    int e = n-1;
-    quickSort(arr,s,e);
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout << endl;
-    return 0;
+//     int s = 0;
+//     int e = n-1;
+//     quickSort(arr,s,e);
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout << endl;
+//     return 0;
 
-}
+// }
